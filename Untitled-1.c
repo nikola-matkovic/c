6160e1993d5a16c1c8adbed17479847ccea9ae0f
bimmerboyy <tarikkucevic@example.com>
2021-11-03 21:51:07 +0100

    stepenovanje

---
+++ Untitled-1.c
@@ -0,0 +1,33 @@
+
+/*
+
+1.promrnljiva x ubdekadnom sistemu 
+2.realne promenljive x i y 
+3.promenljiva x u dekadnom,oktalnom i heksdecimalnom sistemu
+4.stampa vrednost promenljive x tipa char
+
+
+#include <stdio.h>
+#include<stdilb.h>
+
+int main(){
+
+  int x,y;
+  printf("Uneti promenljivu x u dekadnom sistemu:",x);
+  scanf("%d", &x);
+  printf("Uneti realne promenlhive  x i y:",x,y);
+  scanf("%f %f ", &x &y);
+  printf("Uneti promenljivu x u dekadnom heksadekadnom oktalnom sistemu:",x);
+  scanf("%d %x %o", &x &x &x);
+  printf("Istampati vrednost x u char:",x);
+  scanf("%c", &x);
+  system("PAUSE");
+  
+    return 0;
+}
+
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
---
+++ stepenovanje.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+ int a,b;
+printf("Uneti dva broja a i b:");
+scanf("%d %d", &a, &b);
+int c=pow(a,b);
+printf("Dobili smo %d:",c);
+return 0; 
+}
