c8963244f4bc6373d392792b0678ad4174b53a12
bimmerboyy <tarikkucevic@example.com>
2021-11-03 21:24:48 +0100

    ispusio sam karu

---
+++ test.c
@@ -0,0 +1,51 @@
+/*
+
+#include <stdio.h>
+
+int main(){
+
+  int x,y;
+  printf("Uneti promenljivu x u dekadnom sistemu:",x);
+  scanf("%d", &x);
+  printf("Uneti realne promenlhive  x i y:",x,y);
+  scanf("%f %f ", &x &y);
+  printf("Uneti promenljivu x u dekadnom heksadekadnom oktalnom sistemu:",x);
+  scanf("%d %x %o", &x &x &x);
+  printf("Istampati vrednost x u char:",x);
+  scanf("%c", &x);
+  system("PAUSE");
+  
+    return 0;
+}
+*/
+
+
+/*Naci i ispistai na standardnom izlazu zbir.razliku,proizvod i kolicnik dva uneta broja
+
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Unesite dva broja a i b:",a,b);
+    scanf("%d %d", &a,&b);
+    int zbir=a+b;
+    int razlika=a-b;
+    int proizvod=a*b;
+    int kolicnik=a/b;
+    
+    printf("Zbir brojeva %d i %d je:\n",zbir);
+      printf("Razlika brojeva %d i %d je:\n",razlika);
+        printf("Proizvod brojeva %d i %d je:\n",proizvod);
+          printf("Zbir brojeva %d i %d je:\n",kolicnik);
+
+
+
+    
+
+    
+    
+}
+*/
+
+
