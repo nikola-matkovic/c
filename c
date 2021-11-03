77d48701e9b0e428f8c74886f17f1903d6884371
anonymousvegan <majjunjulavgust@gmail.com>
2021-11-03 16:04:24 +0100

    sada c  briše program nakon kompajliranja

--- c
+++ c
@@ -2,3 +2,4 @@ gcc $1 -o $2
 echo -e "\n"
 ./$2
 echo -e "\n"
+rm -rf $2
--- prvi.c
+++ prvi.c
@@ -2,13 +2,13 @@
 int main(){
     int opseg, i;
     do{
-        printf("Unesite zadati opseg, manji od 100");
+        printf("Unesite zadati opseg, manji od 100\n ");
         scanf("%d", &opseg);
     }
-    while (opseg<=100 || opseg>3);
+    while (opseg>100 || opseg<3);
     for(i=3; i<=opseg; i++){
         if(i%4==0){
-            printf("%d", i);
+            printf("%d ", i);
         }
     }
 }
