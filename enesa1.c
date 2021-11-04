ab42525e4ee8de7ef514c06b25ab624876b20d5c
anonymousvegan <majjunjulavgust@gmail.com>
2021-11-04 19:40:38 +0100

    enesa kidaaaa

---
+++ enesa1.c
@@ -0,0 +1,12 @@
+#include  <stdio.h>
+
+int main(){
+    int x=5; //celi brojevi
+    float y=10.4; // decimalani brojevi
+    char prvo_slovo_imena='E'
+
+
+    printf("Enesa  je haker");
+
+    return 0;    
+}
---
+++ enesa2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    // umesto %d može %i
+    // %c - char
+    // %f - float
+    // %i - int
+    printf("Molimo vas unesite prvi broj : ");
+    scanf("%i", &a);
+    printf("Molimo vas unesite drugi broj : ");
+    scanf("%i", &b);
+    c= a+b;
+    printf("Zbir brojeva %d i %d je %d", a,b,c);
+    return 0;
+}
