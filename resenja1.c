7def3d0b87b4758f0bfa6802fd7892cc651b3518
anonymousvegan <majjunjulavgust@gmail.com>
2021-11-09 12:03:09 +0100

    totalno nemam pojma šta radim

--- resenja1.c
+++ resenja1.c
@@ -3,30 +3,40 @@
 // Na primer za pet brojeva 6, 6, 7, 6, 2 ispisuje: Druga po veličini vrednost je 6
 #include <stdio.h>
 int main(){
-    int n, i, prethodni, najveci, t, prvi=0, drugi=0, dva_razlicita;
+    int n, i, prethodni, najveci, najmanji;
+
     do{
     printf("unesite broj n=");
     scanf("%d",&n);
     printf("N je jedanko : %d \n", n);
     }
     while(n<2); 
+
+
     int niz[n];
-    //do{
+    do{
         printf("Unosite broj jedan po jedan:\n");
-        najveci=0;
-        prethodni=0;
         for(i=0;i<n; i++){
             scanf("%d",&niz[i]);
-            if(n==2)
-                najveci==niz[i];
-            if(najveci<niz[i]){
-                prethodni=najveci;
+            if(i==0){
                 najveci=niz[i];
+                najmanji=niz[i];
             }
+            else{
+                if(niz[i]<najmanji){
+                    najmanji=niz[i];
+                }
+                if(niz[i]>najveci){
+                    prethodni=najveci;
+                    najveci=niz[i];
+                }
+                if(i==1){
+                    prethodni=najmanji;
+                }
+            }
+            printf("najveći broj je %d a najmanji je %d. Prethodni od najvećeg je %d \n", najveci, najmanji, prethodni);
         }
-    //}
-    //while(dva_razlicita==0);
-    printf("Resenje: %d", prethodni);
+    }
+    while(najmanji==najveci);
     return 0;
 }
-//Program nije gotov!!! radi za 1-2 ne za 2-1
