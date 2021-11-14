b5c34f0046c8bb4d94a8858ff62bb3c261ed91e5
bimmerboyy <tarikkucevic@example.com>
2021-11-14 22:36:48 +0100

    vezbanje05

---
+++ vezbanje05.c
@@ -0,0 +1,32 @@
+/*2.28. Саставити програм који учитава вредност производа у динарима, а затим израчунава и
+приказује колико је потребно новчаница од 500 дин., 100 дин. и 1 дин. за плаћање тог производа. */
+
+#include<stdio.h>
+
+int main(){
+    int broj,n1,n2,n3,t;
+    n1=500;
+    n2=100;
+    n3=1;
+    printf("Unesite broj :");  // predpostavimo da smo uzeli 1763
+    scanf("%d %d",&broj,&t);
+    t=broj/n1;
+    printf("Novcanica od 500din se u vrednosti proizvoda nalazi %d puta\n",t);
+    t=(broj%n1)/n2;
+    printf("Novcanica od 100din se u preostaloj vrednosti proizvoda nalazi %d puta\n",t);
+    t=((broj%n1)%n2);
+    printf("Novcanica od 1din se u preostaloj vrednosti proizvoda nalazi %d puta\n",t);
+    return 0;
+    }
+
+
+
+    
+ 
+
+
+
+    
+
+ 
+
