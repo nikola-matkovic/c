35a9db2c06e34b1ca9d8477b35a098743cb57af3
bimmerboyy <tarikkucevic@example.com>
2021-11-14 21:44:55 +0100

    taka je bot

---
+++ vezbanje.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int ceo ;
+    float realan;
+    printf("Uneti realan br:\n");
+    scanf("%f",&realan);
+    printf("Uneti ceo broj:\n");
+    scanf("%d",&ceo);
+    printf("\n Realan broj je:\n %f",realan);
+    printf("\nCeo broj je:\n %d",ceo);
+    return 0;
+}
