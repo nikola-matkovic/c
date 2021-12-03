ad33d9a8e79af0961df491902c6601946d9987ce
bimmerboyy <tarikkucevic@example.com>
2021-12-03 08:20:08 +0100

    added js

---
+++ vezbanje58.c
@@ -0,0 +1,8 @@
+/* Ispisati koliko je brojeva intervala(a,b) deljivo sa svojim ciframa (ne uzimajuci u obzir svoju cifru)
+prilikom unosa voditi racuna da je a<b*/
+#include<stdio.h>
+int main(int argc,char *argv[]){
+    int a,b;
+    printf("Uneti broj  ")
+    
+}
