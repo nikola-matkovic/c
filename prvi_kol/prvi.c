46b55a25f9723b76d076ad4c48f68d5e6a45c5b5
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-14 20:01:05 +0100

    prvi

--- prvi_kol/prvi.c
+++ prvi_kol/prvi.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
-int main(){
+void main(){
     float x, y;
     printf ("Unesite x, y");
     scanf ("%f %f", &x, &y);
-    if(y>1 && y<=2./3. * x + 5. / 3. && y<= -2*x+7){
+    if(y>1 && y<=2./3. * x + 5. / 3. && y<= -2*x+7)
         printf("Tačka se nalazi u trouglu");
-    }
-    else {
-        printf("tačka se ne nalazi u trouglu ");
-    }
-    return 0;
+    else 
+       printf("tačka se ne nalazi u trouglu ");
 }
