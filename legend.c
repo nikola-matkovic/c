846562869ffafa45460dbc8532b9c6a57a15cd0b
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-16 20:54:08 +0100

    legend

--- legend.c
+++ legend.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-int main(){
+void main(){
     int n;
     printf("Unesite broj za proveru: ");
     scanf("%d", &n);
     (n & 1) ? printf("Uneti broj je paran\n") : printf("Uneti broj je paran\n");
-    return 0;
 }
