39cddaa769d18cb33ea1510a6966dbf08762b1ea
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-18 23:08:57 +0100

    iskomentarisao

--- nzs_nzd.c
+++ nzs_nzd.c
@@ -5,24 +5,24 @@
 */
 #include <stdio.h>
 
-int nzd_f(int a, int b){
-	int s = 1;
-	int i = 2;
-	while(i <= a && i <= b){
-		while(a % i == 0 && b % i == 0 ){
-			s *= i;
+int nzd_f(int a, int b){             // 1. funkcija za izračunavanje nzd-a
+	int s = 1;							// 2. s je jedan jer se množi ( neutralan element)
+	int i = 2;							// 3. deljenje startaš sa 2
+	while(i <= a && i <= b){			// 5. to sve treba da ponavlja dok je i manje od a i b, kada bude veće kraj programa (jer broj nije deljiv brojem većim od sebe)
+		while(a % i == 0 && b % i == 0 ){ //4.dok su oba deljiva sa 2  deliš ih i  množiš s,  kada dođe do kraja  i će se uvećati i probati da deli sa 3, 4....
+			s *= i;														
 			a /= i;
 			b /= i;
 		}
 		i++;
 	}
-	return s;
+	return s;							//6 funkcija vraća s
 }
-int nzs_f(int a, int b){
-	int nzd = nzd_f(a, b);
-	a /= nzd;
-	b /= nzd;
-	return nzd * a * b;
+int nzs_f(int a, int b){               // 7 nzs je samo nzd  pomnožen sa  brojevima koji su ostali nepodeljieni
+	int nzd = nzd_f(a, b);				//8 računa nzd
+	a /= nzd;							// 9 nalazi koliko je  a pri računananju nzd-a	
+	b /= nzd;							// isto i za  b 		
+	return nzd * a * b;					// množi sve to da bi dobio nzs
 }
 void main(){
 	int a, b, nzs, nzd;
