77926e265552fa7b42acfc9005a0a1138ccfa422
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-18 14:02:46 +0100

    update

--- noob_vs_pro.c
+++ noob_vs_pro.c
@@ -4,7 +4,8 @@ int main(){
     printf("Unesite broj za proveru: ");
     scanf("%d", &n);
     //noob
-    if(n % 2 == 0){
+    if(n % 2 == 0)
+    {
         printf("Uneti broj je paran\n");
     }
     else
@@ -19,6 +20,6 @@ int main(){
     //pro
     (n % 2 == 0) ? printf("Uneti broj je paran\n") : printf("Uneti broj je paran\n");
     //LEGEND
-    (n & 1) ? printf("Uneti broj je paran\n") : printf("Uneti broj je paran\n");
+    (n & 1) ? printf("Uneti broj je neparan\n") : printf("Uneti broj je paran\n");
     return 0;
 }
