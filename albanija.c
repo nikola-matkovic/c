ce027e7fa51b996843d0fc2acf57398358956698
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-22 21:59:35 +0100

    albanija

---
+++ albanija.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  	char nacija[15];
+	char ime[15];
+	printf("Unesite naciju: ");
+	scanf("%s", nacija );
+	printf("Unesite ime:");
+	scanf("%s", ime);
+	printf("Vi se zovete  %s, i vaša nacionalna pripadnost je srpska, samo vam niko nije rekao!, svi ste vi srbi breeeeeee", ime, nacija);
+}
