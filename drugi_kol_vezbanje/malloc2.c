f67f01c8b18dc6fe340d2d78c2d26400a81818df
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-28 22:45:42 +0100

    why

---
+++ drugi_kol_vezbanje/malloc2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int* niz = (int*)malloc(sizeof(int));
+    int broj = 1;
+    int i = 0;
+    while(broj !=0)
+    {
+        scanf("%d", &broj);
+        niz = realloc(niz, (i+1)*sizeof(int));
+        *(niz+i) = broj;
+        i++;
+    }
+    printf("Ispis niza");
+    
+    for(int j = 0; j<i; j++){
+        printf("%d \n", *(niz+j));
+    }
+    free(niz);
+    return 0;
+}
--- drugi_kol_vezbanje/maloc.c
+++ drugi_kol_vezbanje/maloc.c
@@ -1,8 +1,20 @@
 #include <stdio.h>  
-#include <conio.h>  
-#include <ctype.h>   
+#include <stdlib.h>
 int main()    
 {  
-    int 
+    int* niz = (int*) malloc(sizeof(int));
+    int broj = 1;
+
+    int i = 0, j=0;
+    while(broj !=0){
+        scanf("%d", &broj);
+        niz = realloc(niz, (i+1) * sizeof(int));
+        *(niz+1) = broj;
+        i++;
+    }
+    for(int j = 0; j < i; j++){
+        printf("%d\n", *(niz+j));
+    }
+    free(niz);
     return 0;
 }  
