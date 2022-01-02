751e9a5f563d066fc4af01d9a0d00ae27df67d08
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-02 17:13:56 +0100

    reši

--- end.c
+++ end.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 int main(){
     int godina = 2021; 
     godina++;
 
-    printf("Srećna nova %d godina svima koji slave <3 ", godina);
+    printf("Srećna nova %d godina svima koji slave <3 :) ", godina);
     return  0;
 }
