5115ebe19f84cdfd52cea8380e4165e250765ceb
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-09 21:58:13 +0100

    reši

--- zmija.c
+++ zmija.c
@@ -3,17 +3,56 @@
 #include <time.h>
 #include <stdlib.h>
 
-int m, n, i, j, rx, ry;
+#define MAX 100
 
-int zmija []
+typedef struct zmija{
+    int x; 
+    int y;
+}ZMIJA;
+
+typedef struct hrana{
+    int x; 
+    int y;
+}hrana;
+
+
+int m, n, i, j, rx, ry, d=0;
+
+ZMIJA z[MAX];
+
+hrana h;
 
 void nasumicno(){
-    rx = (rand() % (n- 1 + 1)) + 1;
-    ry = (rand() % (m- 1 + 1)) + 1;
+    ry = (rand() % m) + 1;
+    rx = (rand() % n) + 1;
+}
+
+void postavi_zmiju(){
+    if(d == 0){
+        nasumicno();
+        z[d].x = rx;
+        z[d].y = ry;
+    }
+    d++;
+}
+
+void postavi_hranu(){
+    nasumicno();
+    for(i=0; i<d; i++) {
+        if(z[0].x == rx && z[0].y == ry ){
+            postavi_hranu();
+        }
+        else{
+            h.x = rx;
+            h.y = ry;
+        }
+    }
 }
 
 void napravi_igraliste(){
     scanf("%d %d", &m, &n);
+    postavi_zmiju();
+    postavi_hranu();
     m+=2; n+=2;
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
@@ -23,16 +62,22 @@ void napravi_igraliste(){
             else if(j==0 || j==n-1){
                 printf("#");
             }
-            else{
-                 printf(" ");
+            else if(i==z[0].y && j==z[0].x){
+                    printf("*");
             }
-    
+            else if(i==h.y && j==h.x){
+                    printf("@");
+            }
+            else{
+                //postavi_hranu();
+                printf(" ");
+            }
         }
         printf("\n");
     }
 }
 
-void  main(){
+void main(){
     srand(time(0));
     napravi_igraliste();
 }
