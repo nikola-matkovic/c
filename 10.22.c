609972d328a92db57618d391593e970f11ac0a3a
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-22 16:59:51 +0100

    i'm trying

--- 10.22.c
+++ 10.22.c
@@ -1,16 +1,17 @@
 /*10.22. Саставити функцију којом се одређује број различитих елемената у задатом целобројном
 низу. Затим саставити програм који чита низ целих бројева, и одређује број различитих елемената
 користећи претходну функцију и исписује резултат. */
-
 #include <stdio.h>
 
-void razliciti(int niz[]){
-    int i=0;
+void razliciti(int niz[], int velicina){
+   int razliciti[velicina];
+   int i, j;
+   niz[]
 }
 
 void main(){
+    int i=0;
     int a[] = {1, 2, 1, 3, 1, 4 };
-    int length;
-    length = sizeof(a) / sizeof(int);
-    printf("%d", length);
+    int velicina = sizeof(a) / sizeof(int);
+    razliciti(a, velicina);
 }
---
+++ foreach_bičz.js
@@ -0,0 +1,3 @@
+let a = [1, 2, 3, ,4, 5, 6, 7, 7, 8 ,1, 2]
+
+a.forEach(e => console.log(e))
