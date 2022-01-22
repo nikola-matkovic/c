478d8b673c015ade0388d235058274b9fb939dfb
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-22 17:32:11 +0100

    lutko ja sam rešen da večno stobom plešem

--- 10.22.c
+++ 10.22.c
@@ -3,15 +3,35 @@
 користећи претходну функцију и исписује резултат. */
 #include <stdio.h>
 
-void razliciti(int niz[], int velicina){
-   int razliciti[velicina];
-   int i, j;
-   niz[]
+int razliciti(int niz[], int velicina1){
+    int razliciti[velicina1];
+    int velicina2 = 0;
+    int i=0, j=0;
+    razliciti[j]  = niz[0];
+    velicina2++;
+    int zastava;
+    for(i=1; i<velicina1; i++){
+        zastava = 1;
+        for(j=0; j<velicina2; j++){
+            if(razliciti[j]==niz[i]){
+                zastava = 0;
+            }
+        }
+        if(zastava){
+            razliciti[velicina2] = niz[i];
+            velicina2++; 
+        }
+    }
+    return velicina2;
 }
 
 void main(){
-    int i=0;
-    int a[] = {1, 2, 1, 3, 1, 4 };
-    int velicina = sizeof(a) / sizeof(int);
-    razliciti(a, velicina);
+    int i=0, velicina;
+    printf("Koliko elemenata želite da unesete ?");
+    scanf("%d", &velicina );
+    int a[velicina];
+    for(i=0; i<velicina; i++){
+        scanf("%d", &a[i]);
+    }
+    printf("u datom nizu postoji %d različitih elemenata", razliciti(a, velicina));
 }
