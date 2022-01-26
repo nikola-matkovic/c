ebe5e3553abf1ebea042c5a94824486f842d7840
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-26 18:28:59 +0100

    reši

--- nizovi-vezbanje/10.29.c
+++ nizovi-vezbanje/10.29.c
@@ -2,24 +2,23 @@
 вредности међу парним бројевима. */
 
 #include<stdio.h>
-int main(){
-    int i,n,m,niz1[m],min;
+void main(){
+    int i,n,min, j = 0;
     printf("Unesite n:");
     scanf("%d",&n);
-    int niz[n];
+    int niz[n], niz_parnih[n];
     for(i=0;i<n;i++){
         scanf("%d",&niz[i]);
-        for(i=0;i<n;i++){
-            if(niz[i]%2){
-                niz[i]=niz[m];
-            }
-            for(i=0;i<m;i++){
-                niz[0]=min;
-                if(niz[i]<min){
-                    min=niz[i];
-                }
-            }
+        if(niz[i] % 2 == 0){
+            niz_parnih[j] = niz[i];
+            j++;
         }
     }
-    return 0;
+    min = niz_parnih[0];
+    for(i=0; i<j; i++){
+        if(niz_parnih[i] < min){
+            min = niz_parnih[i];
+        }
+    }
+    printf("%d", min);
 }
