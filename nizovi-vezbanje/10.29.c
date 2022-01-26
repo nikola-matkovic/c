6cb3ff7421c43d7ee8da5ad693630ba996c79838
anonymousvegan <majjunjulavgustgit@gmail.com>
2022-01-26 17:18:26 +0100

    nisam otero

---
+++ nizovi-vezbanje/10.29.c
@@ -0,0 +1,24 @@
+/*10.29. Саставити програм који за унети низ целих бројева, дужине n, исписује елемент најмање
+вредности међу парним бројевима. */
+
+#include<stdio.h>
+int main(){
+    int i,n,niz[n],m,niz1[m],min;
+    printf("Unesite n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&niz[i]);
+        for(i=0;i<n;i++){
+            if(niz[i]%2){
+                niz[i]=niz[m];
+            }
+            for(i=0;i<m;i++){
+                niz[0]=min;
+                if(niz[i]<min){
+                    min=niz[i];
+                }
+            }
+        }
+    }
+    return 0;
+}
