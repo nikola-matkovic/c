b992bfbb25dc0a1b807f189d9a50886c5bf8355b
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-26 17:52:28 +0100

    radi

--- nizovi-vezbanje/10.28-moja_verzija.c
+++ nizovi-vezbanje/10.28-moja_verzija.c
@@ -1,30 +1,30 @@
 /*10.28. Саставити програм који учита низ реалних бројева, дужине n, налази најмањи и највећи члан
 низа, xmin и xmax, и исписује све елементе низа који су мањи од xmax/2 и већи од xmin*2.*/
 
-#include<stdio.h>
-int main(){
-    int n,i,max,min;
+#include <stdio.h>
+
+void main(){
+    int n, i, min, max;
     printf("Unesite n:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int niz[n];
-    for (i=0;i<n;i++){
-        scanf("%d",&niz[i]);
+    printf("Unesite elemente niza:");
+    for(i=0; i<n; i++){
+        scanf("%d", &niz[i]);
         if(i == 0){
+            min = niz[i]; 
             max = niz[i];
+        }
+        if(niz[i]<min){
             min = niz[i];
         }
         if(niz[i]>max){
-            max=niz[i];
-        }
-        if(niz[i]<min){
-            min=niz[i];
+            max = niz[i];
         }
     }
-    printf("ELementi koji su manji od %.2f i veći od %d su: ", (float) max / 2, min * 2);
-    for(i = 0;i < n; i++){
-        if(niz[i] < (float) max / 2 && niz[i] > min * 2){
-            printf("%d ", niz[i]);
-        }
+    printf("мањи од %f и већи од %d су ", (float) max / 2, min * 2);
+    for(i=0; i<n; i++){
+        if(niz[i] > min * 2 && niz[i] < (float) max / 2)
+            printf("%d, ", niz[i]);
     }
-    return 0;
 }
