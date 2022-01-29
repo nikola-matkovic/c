c52cc446c30da37cebcc035bf1f982027eef363b
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-29 22:02:42 +0100

    traženje jedinstvenih elemenata kroz nis

--- nizovi-vezbanje/10.38.c
+++ nizovi-vezbanje/10.38.c
@@ -1,19 +1,34 @@
 /*10.38. Саставити програм који за унети низ целих бројева дужине n формира и приказује нови низ
 који је састављен од елемената без понављања унетог низа.*/
 
-#include<stdio.h>
+#include <stdio.h>
+
 int main(){
-    int i,n,a;
+    int i, n, a, j=0, brojElemenata=0, zastava;
+    //unos niza
     printf("Unesite n:");
     scanf("%d",&n);
-    int A[n],B[n];
-    for(i=0;i<n;i++){
+    int A[n], B[n];
+    for(i = 0; i < n; i++){
         scanf("%d",&A[i]);
     }
-    a=A[0];
-    for(i=1;i<n;i++){
-       {
-            
+    //stvaranje novog niza:
+    for(i = 0; i < n; i++){ // prelai kroz sve elemente niza 
+        zastava = 0;        // za svaki prepostavimo da nije našao isti
+        for(j=0; j<brojElemenata; j++){ // proverava redukovani niz, da li se  element već nalazi u njemu 
+            if(B[j]==A[i]){
+                zastava = 1;            //ako se nalazi, postavlja zastavu na 1
+                break;
+            }
+        }
+        if(!zastava){                    //ako element nije pronađen u nizu, tj ako se ne ponavlja, upiše ga
+            B[brojElemenata] = A[i];
+            brojElemenata++;
         }
     }
+    //ispis novog niza
+    for(i = 0; i < brojElemenata; i++){
+        printf("%d ", B[i]);
+    }
+    return 0;
 }
