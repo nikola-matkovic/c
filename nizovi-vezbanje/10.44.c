4ee57aa51a38e29cd7dfbad63005ac93303a6604
anonymousvegan <majjunjulavgust@gmail.com>
2022-02-02 18:15:17 +0100

    reši

--- nizovi-vezbanje/10.44.c
+++ nizovi-vezbanje/10.44.c
@@ -3,25 +3,25 @@
 секвенцу једнаких елемената. Низ се уноси са тастатуре.*/ 
 #include<stdio.h>
 
-
 int main(){
-  int i,n;
-  printf("Unesite n:);
-  scanf("%d",&n);
-  int niz[n];
-  printf("Unesite niz:);
-  for(i=0;i<n;i++){
+   int i,n;
+   printf("Unesite n:);
+   scanf("%d",&n);
+   int niz[n];
+   printf("Unesite niz:);
+   for(i=0;i<n;i++){
       scanf("%d",&niz[i]);
-     }
-  for(i=0;i<n;i++){
-     for(j=i+1;j<n;j++){
-        if(niz[j]<niz[i]){
-     	   niz[i]=niz[i]^niz[j];
-           niz[j]=niz[i]^niz[j];
-           niz[i]=niz[i]^niz[j];
-        }
-     }
    }
+   for(i=0;i<n;i++){
+      for(j=i+1;j<n;j++){
+         if(niz[j]<niz[i]){
+            niz[i]=niz[i]^niz[j];
+            niz[j]=niz[i]^niz[j];
+            niz[i]=niz[i]^niz[j];
+         }
+      }
+   }
+
    for(i=0;i<n;i++){
 	printf("%d",&niz[i]);
 }
