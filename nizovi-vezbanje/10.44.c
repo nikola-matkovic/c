6fb73583768d1be85bb4ff69aa1f001b58aa69d3
anonymousvegan <majjunjulavgust@gmail.com>
2022-02-02 18:31:33 +0100

    provera ponavljanja u nizu

--- nizovi-vezbanje/10.44.c
+++ nizovi-vezbanje/10.44.c
@@ -4,14 +4,16 @@
 #include<stdio.h>
 
 int main(){
-   int i,n;
-   printf("Unesite n:);
-   scanf("%d",&n);
-   int niz[n];
-   printf("Unesite niz:);
-   for(i=0;i<n;i++){
-      scanf("%d",&niz[i]);
-   }
+   int i,n, br, t, p, j;
+   // printf("Unesite n:);
+   // scanf("%d",&n);
+   // int niz[n];
+   // printf("Unesite niz:);
+   // for(i=0;i<n;i++){
+   //    scanf("%d",&niz[i]);
+   // }
+   n = 13;
+   int niz[] = {1, 2, 2, 3, 3, 4, 4, 4, 5, 6, 6, 6, 6 };
    for(i=0;i<n;i++){
       for(j=i+1;j<n;j++){
          if(niz[j]<niz[i]){
@@ -21,8 +23,23 @@ int main(){
          }
       }
    }
+   br = niz[0];
+   p = 0;
+   for(i=1;  i<n; i++){
+      j = i+1;
+      t = 1; 
+      while(niz[j] == niz[i]){
+         t++;
+         j++;
+      }
+      if(t > p){
+         p = t;
+         br = niz[i];
+      }
+   }
+   printf("najviše puta se ponavlja broj %d, i to %d puta", br, p);
 
-   for(i=0;i<n;i++){
-	printf("%d",&niz[i]);
+   // for(i=0;i<n;i++){
+	// printf("%d",&niz[i]);
 }
 
