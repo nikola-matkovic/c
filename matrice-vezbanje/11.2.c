3419eb79b110e82480d6b1493893968198caf4ba
bimmer <majjunjulavgustgit@gmail.com>
2022-02-06 14:00:39 +0100

    ja ne znam sta sam upravo napisao

--- matrice-vezbanje/11.2.c
+++ matrice-vezbanje/11.2.c
@@ -5,7 +5,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-    int m,n,i,j;
+    int m,n,i,j,paran_1,paran_2,paran_3,paran_4,paran_5,paran_6,paran_7,paran_8,paran_9;
+    int obican_1, obican_2, obican_3, obican_4, obican_5, obican_6, obican_7, obican_8, obican_9,k,z,semafor=0;
     printf("Unesite m:");
     scanf("%d",&m);
     printf("Unesite n:");
@@ -17,6 +18,96 @@ int main(){
             scanf("%d",&matrix[i][j]);
         }
     } 
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            while(i==0 && j==0){
+                if(matrix[i][j] % 2 == 0){
+                    paran_1=matrix[i][j];
+                }
+                else {
+                obican_1=matrix[i][j];
+                }   
+            }
+            while(i==0 && j==1){
+                if(matrix[i][j] % 2 == 0){
+                    paran_2=matrix[i][j];
+                }
+                else{
+                    obican_2=matrix[i][j];
+                    }
+            }
+            while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_3=matrix[i][j];
+                }
+                else{
+                    obican_3=matrix[i][j];
+                }
+                while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_4=matrix[i][j];
+                }
+                else{
+                    obican_4=matrix[i][j];
+                }
+            }
+            while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_5=matrix[i][j];
+                }
+                else{
+                    obican_5=matrix[i][j];
+                }
+                while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_6=matrix[i][j];
+                }
+                else{
+                    obican_6=matrix[i][j];
+                }
+                while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_7=matrix[i][j];
+                }
+                else{
+                    obican_7=matrix[i][j];
+                }
+                while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_8=matrix[i][j];
+                }
+                else{
+                    obican_8=matrix[i][j];
+                }
+                while(i==0 && j==2){
+                if(matrix[i][j] % 2 == 0){
+                    paran_9=matrix[i][j];
+                }
+                else{
+                    obican_9=matrix[i][j];
+                }
+            for(k=0;k<paran_9;k++){
+                for(z=0;z<obican_9;z++){
+                    if(semafor=1){
+                    paran_1+=paran_2;
+                    paran_2+=paran_3;
+                    paran_3=paran_4;
+                    paran_4+=paran_5;
+                    paran_5+=paran_6;
+                    paran_6+=paran_7;
+                    paran_7+=paran_8;
+                    paran_8+=paran_9;
+
+                    }
+                  
+
+
+                }
+                
+            }
+            
+        }
+    }
 
 
 
