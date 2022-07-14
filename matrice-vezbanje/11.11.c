d332358e6de8820b33f70ae20c427fc96b31a386
bimmer <tarikkucevic@gmail.com>
2022-07-14 15:10:24 +0200

    ok

--- matrice-vezbanje/11.11.c
+++ matrice-vezbanje/11.11.c
@@ -15,7 +15,17 @@ int main(){
         for(j = 0; j < n; j++){
             scanf("%d",&matrix[i][j]);
         }
-    }    
+    }
+    printf("\n");
+    for(i = 0; i < m; i++){
+        for(j = 0; j < n; j++){
+            
+            printf("%d ",matrix[i][j]);
+        }
+       
+        printf("|\t");
+    }
+        
 
     return 0;
 }
