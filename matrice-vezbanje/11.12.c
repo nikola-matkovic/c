46261d35f8c66c63c4a3568c720305f51728bd42
bimmer <tarikkucevic@gmail.com>
2022-07-14 19:00:58 +0200

    ok

--- matrice-vezbanje/11.12.c
+++ matrice-vezbanje/11.12.c
@@ -14,7 +14,13 @@ int main(){
     printf("Uensite n:");
     scanf("%d",&n);
     int matrix[n][n];
-    for()
+    printf("Unesite matricu:\n");
+    for(i = 0;i < n;i++){
+        for(j = 0;j < n;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    
 
     return 0;
 }
