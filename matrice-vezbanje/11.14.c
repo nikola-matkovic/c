e80afe7c83ae1b9054744bfd64a8e766564b3d6f
bimmer <tarikkucevic@gmail.com>
2022-07-15 00:52:01 +0200

    ok

---
+++ matrice-vezbanje/11.14.c
@@ -0,0 +1,34 @@
+// 11.17. Саставити програм који учита матрицу реалних бројева A димензија nxn и, а затим дату
+// матрицу своди на горњу десну троугласту. Исписати новодобијену матрицу.
+
+#include<stdio.h>
+
+int main(){
+    int n,i,j;
+    printf("Unesite n:");
+    scanf("%d",&n);
+    int matix[n][n];
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            scanf("%d",&matix[i][j]);
+        }    
+    }
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            printf("%d",matix[i][j]);
+        }    
+    }
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            if(j > 0 && j < n){
+                j = j - 1 - i;
+                printf("%d",matix[i][j]);
+            }
+            printf("\n");
+        }    
+    }
+    
+
+
+    return 0;
+}
