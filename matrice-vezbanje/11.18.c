0007c3072ca131e0f604e2b985b52b4b61db79b5
bimmer <tarikkucevic@gmail.com>
2022-07-16 14:28:32 +0200

    ok

--- matrice-vezbanje/11.18.c
+++ matrice-vezbanje/11.18.c
@@ -10,7 +10,31 @@ int main(){
     scanf("%d",&m);
     printf("Uneiste n:");
     scanf("%d",&n);
-    
+    int matrix[m][n];
+    int zbir[m];
+    //Unos matrice
+    printf("Unesite matricu:");
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    //Ispis matrice
+    printf("Zadata matrica je:\n");
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            printf("%d",matrix[i][j]);
+        }
+    }
+    //Zbir elemenata po kolni
+    printf("Zbir elemenata po kolni:\n");
+    for(i = 0; i < n; i++){
+        zbir[i] = 0;
+        for(j = 0; j < m; j++){
+            zbir[i] += matrix[i][j];
+            
+        }
+    }
 
     return 0;
 }
