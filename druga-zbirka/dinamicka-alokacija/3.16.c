6c81d51ab763f15eb2317947ebaeef66c8a06bab
bimmer <tarikkucevic@gmail.com>
2022-07-18 19:24:50 +0200

    sta je trik

--- druga-zbirka/dinamicka-alokacija/3.16.c
+++ druga-zbirka/dinamicka-alokacija/3.16.c
@@ -5,6 +5,7 @@
 #include<stdlib.h>
 
 int main(){
+    system("clear");
     int *s1,*s2,*s3;
     int n1,n2,n3,i,j;
     printf("Unesite duzinu za s1:");
@@ -21,13 +22,32 @@ int main(){
     for(int i = 0; i < n2; i++){
         scanf("%d",&s2[i]);
     }
-     printf("Unesite duzinu za s3:");
-    scanf("%d",&n3);
-    printf("Unesite  elmente za s3\n");
+  
+    
+    //Unija skupova
+    n3 = n1 + n2;
     s3 = (int*)malloc(n3*sizeof(int));
-    for(int i = 0; i < n3; i++){
-        scanf("%d",&s3[i]);
+    for(int i = 0; i < n1; i++){
+        s3[i] = s1[i];
     }
+    int br = n1-1;
+    // s3 = (int*)realloc(s3, n3*sizeof(int));
+    for(int j = 0; j < n2; j++){
+        for(int i = 0; i < n1; i++){
+            if(s1[i] != s2[j]){
+                br++;
+                s3[br] = s2[j];
+            }
+        }
+    }
+    printf("\nIspis za unije dva skupa:\n");
+     for(int i = 0; i < n3; i++){
+         printf("%d ",s3[i]);
+     }
+
+   
+   
 
     return 0;
+   
 }
