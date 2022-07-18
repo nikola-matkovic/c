ef13fe52a3ffcf9c1b265ed22c0ccf632b0afad1
bimmer <tarikkucevic@gmail.com>
2022-07-18 14:53:47 +0200

    ok

--- druga-zbirka/stringovi/dinamicka-alokacija/3.4.c
+++ druga-zbirka/stringovi/dinamicka-alokacija/3.4.c
@@ -5,5 +5,12 @@
 #include<stdlib.h>
 
 int main(){
+    int n,i;
+    printf("Unesite duzinu:");
+    scanf("%d",&n);
+    int niz[n];
+    for(i = 0; i < n; i++){
+        scanf("%d",&niz[i]);
+    }
     return 0;
 }
