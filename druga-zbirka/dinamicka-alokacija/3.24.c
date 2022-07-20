823b890d3466623e8b8766f39571d7928c4ab044
bimmer <tarikkucevic@gmail.com>
2022-07-20 21:13:17 +0200

    determining the determinant

--- druga-zbirka/dinamicka-alokacija/3.24.c
+++ druga-zbirka/dinamicka-alokacija/3.24.c
@@ -4,6 +4,37 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+int Determinanta(int **matrica, int n){
+   
+
+ int **podmatrica, i,j, det=0, znak=1, vrsta, kolona;
+ 
+ podmatrica = (int**)malloc((n-1)*sizeof(int));
+ for(i = 0; i < n; i++){
+    podmatrica[i] = (int*)malloc((n-1)*sizeof(int));
+    for(j = 0; j < n; j++){
+    }
+ }
+for(i=0; i<n; i++){
+    for(kolona=0; kolona<i; kolona++){
+        for(vrsta=1; vrsta<n; vrsta++){
+            podmatrica[vrsta-1][kolona] = matrica[vrsta][kolona];
+        }
+    }
+
+ for(kolona=i+1; kolona<n; kolona++){
+    for(vrsta=1; vrsta<n; vrsta++){
+        podmatrica[vrsta-1][kolona-1] = matrica[vrsta][kolona];
+      
+    }
+    det += znak*matrica[0][i]*Determinanta(podmatrica,n-1);
+    znak*=-1;
+}
+ 
+}
+ return det;
+} 
+
 
 int main(){
 system("clear");
@@ -30,6 +61,9 @@ for(i = 0; i < n; i++){
     }
     printf("\n");
 }
+printf("Determinanta matrice je:\n");
+
+printf("%d",Determinanta(matrix,n));
 
 
     return 0;
