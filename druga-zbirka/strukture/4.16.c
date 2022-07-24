d7ace429a2db7938a7e8e20cfeb906e11843fbba
bimmer <tarikkucevic@gmail.com>
2022-07-24 15:11:27 +0200

    finding a point on a circle with struct

--- druga-zbirka/strukture/4.16.c
+++ druga-zbirka/strukture/4.16.c
@@ -4,6 +4,8 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+#include<stdbool.h>
 
 // Tacka
 
@@ -17,10 +19,50 @@ typedef struct krug{
     float r;
 }KRUG;
 
+float rastojanjeIzmdejuDveTacke(struct tacka *a,struct tacka *b){
+    float d;
+    d = sqrt(pow((b->x - a->x),2) + pow((b->y - a->y),2));
+    return d;
+}
 
+bool daLiSeNalaziUKrugu(struct tacka *tacka,struct krug *krug){
+    if(rastojanjeIzmdejuDveTacke(tacka,&krug->centar) <= krug->r){
+        return true;
 
+    }
+    else{
+        return false;
+    }
+
+}   
 
 int main(){
+    struct tacka A;
+    struct tacka B;
+    struct krug O;
+    printf("Unesite x za tacku A:");
+    scanf("%d",&A.x);
+    printf("Unesite y za tacku A:");
+    scanf("%d",&A.y);
+    printf("Unesite x za tacku B:");
+    scanf("%d",&B.x);
+    printf("Unesite y za tacku B:");
+    scanf("%d",&B.y);
+    printf("Unesite koordinate kruga:");
+    printf("\nx:");
+    scanf("%d",&O.centar.x);
+    printf("\ny:");
+    scanf("%d",&O.centar.y);
+    printf("Unesite poluprecnik za krug O:");
+    scanf("%f",&O.r);
+    printf("\nTacka A i B se nalaze na rastojanju: %f",rastojanjeIzmdejuDveTacke(&A,&B));
+   if(daLiSeNalaziUKrugu(&A,&O) == true){
+    printf("Tacka A se nalazi u krugu");
+   }
+   else{
+    printf("Tacka A se ne nalazi u krugu");
+   }
+    
 
     return 0;
 }
