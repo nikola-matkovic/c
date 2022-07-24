0967889eba4b66f4788d3d40483a97a7c6cacabc
bimmer <tarikkucevic@gmail.com>
2022-07-24 14:39:23 +0200

    ok

---
+++ druga-zbirka/strukture/4.16.c
@@ -0,0 +1,26 @@
+// 4.16. Саставити главни функцију која проверава да ли се задата тачка налази унутар задатог круга.
+// Параметри круга су координате центра и полупречник. Затим саставити програм који тестира
+// претходну функцију. 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+// Tacka
+
+typedef struct tacka{
+    int x;
+    int y;
+}TACKA;
+
+typedef struct krug{
+    struct tacka centar;
+    float r;
+}KRUG;
+
+
+
+
+int main(){
+
+    return 0;
+}
