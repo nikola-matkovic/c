2f6b91a50cd25f4b908c90c792bac96ea1311f73
bimmer <tarikkucevic@gmail.com>
2022-07-24 16:08:46 +0200

    operations with polynoms

--- druga-zbirka/strukture/4.18.c
+++ druga-zbirka/strukture/4.18.c
@@ -10,6 +10,18 @@ typedef struct polinom{
     int eksponent;
 }POLINOM;
 
+int odredjivanjeEksponenta(struct polinom p1,struct polinom p2){
+    int i;
+    struct polinom p;
+    if(p1.eksponent > p2.eksponent){
+        p.eksponent = p1.eksponent;
+    }
+    else{
+        p.eksponent = p2.eksponent;
+    }
+    return p.eksponent;
+}
+
 struct polinom sabiranjeDvaPolinoma(struct polinom p1,struct polinom p2){
     int i;
     struct polinom p;
@@ -27,7 +39,32 @@ struct polinom sabiranjeDvaPolinoma(struct polinom p1,struct polinom p2){
             p.koeficijenti[i] = p2.koeficijenti[i];
         }
         else{
-             p.koeficijenti[i] = p2.koeficijenti[i] + p1.koeficijenti[i];
+            p.koeficijenti[i] = p2.koeficijenti[i] + p1.koeficijenti[i];
+        }
+    }
+    while(p.eksponent >= 0 && p.koeficijenti[p.eksponent] == 0){
+        p.eksponent--;    
+    }
+    return p;
+}
+struct polinom oduzimanjeDvaPolinoma(struct polinom p1,struct polinom p2){
+    int i;
+    struct polinom p;
+    if(p1.eksponent > p2.eksponent){
+        p.eksponent = p1.eksponent;
+    }
+    else{
+        p.eksponent = p2.eksponent;
+    }
+    for(i = 0; i <= p.eksponent; i++){
+        if(i > p2.eksponent){
+            p.koeficijenti[i] = p1.koeficijenti[i];
+        }
+        else if(i > p1.eksponent){
+            p.koeficijenti[i] = p2.koeficijenti[i];
+        }
+        else{
+            p.koeficijenti[i] = p2.koeficijenti[i] - p1.koeficijenti[i];
         }
     }
     while(p.eksponent >= 0 && p.koeficijenti[p.eksponent] == 0){
@@ -37,6 +74,21 @@ struct polinom sabiranjeDvaPolinoma(struct polinom p1,struct polinom p2){
 }
 
 int main(){
+    struct polinom p1;
+    struct polinom p2;
+    int i;
+    printf("Unesite koeficijente polinoma p1");
+    for(i = 0;i < odredjivanjeEksponenta(p1,p2);i++){
+        scanf("%d",&p1.koeficijenti[i]);
+    }
+    printf("Unesite koeficijente polinoma p2");
+    for(i = 0;i < odredjivanjeEksponenta(p1,p2);i++){
+        scanf("%d",&p2.koeficijenti[i]);
+    }
+    printf("Polinom p1 je:\n");
+    for(i = 0;i < odredjivanjeEksponenta(p1,p2); i++){
+        printf("%d",p1.koeficijenti[i]);
+    }
 
     return 0;
 }
