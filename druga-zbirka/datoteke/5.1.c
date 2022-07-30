3e914eddd10b0c6bb0a5c69a793f18a5e46f7f0e
bimmer <tarikkucevic@gmail.com>
2022-07-30 12:48:49 +0200

    works with files

---
+++ druga-zbirka/datoteke/5.1.c
@@ -0,0 +1,37 @@
+// 5.1. Саставити програм којим се у датотеку podaci.txt уписује првих 10 целих бројева , а затим се
+// из исте датотеке читају бројеви док се не достигне до краја датотеке, а затим се бројеви исписују на
+// стандарни излаз (екран). 
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int i,br;
+    FILE *datoteka;
+    datoteka = fopen("podaci.txt", "w");
+    if(datoteka == NULL){
+        printf("Doslo je do greske i datoteka se nije uspela otvoriti");
+        exit(1);
+    }
+    for(i = 0;i < 10;i++){
+        fprintf(datoteka,"%d",i);
+    }
+    fclose(datoteka);
+
+    datoteka = fopen("podaci.txt", "r");
+
+    if(datoteka == NULL){
+        printf("Doslo je do greske i datoteka se nije uspela otvoriti");
+    }
+    while(1){
+        fscanf(datoteka, "%d", &br);
+        if(feof(datoteka)){
+            break;
+        }
+        printf("Procitano:%d",&br);
+
+    }
+    fclose(datoteka);
+
+    return 0;
+}
