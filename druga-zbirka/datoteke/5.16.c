a6b985e6a0d20f5a9712c0c96d0c2043135f6c17
bimmer <tarikkucevic@gmail.com>
2022-08-01 17:51:27 +0200

    removing numbers to other file with as > 0

--- druga-zbirka/datoteke/5.16.c
+++ druga-zbirka/datoteke/5.16.c
@@ -13,9 +13,32 @@ int main(){
     FILE *prva;
     FILE *druga;
     int i,n;
-    float broj[SIZE];
+    float broj[SIZE],s = 0,zbirSrednjeVrednosti = 0;
     prva = fopen("nizA.txt", "r");
     druga = fopen("nizB.txt", "w");
+    if(prva == NULL || druga == NULL){
+        printf("Greska");
+    }
+    printf("Uneiste n:");
+    scanf("%d",&n);
+    while(!feof(prva)){
+        for(i = 0; i < n;i++){
+            fscanf(prva, "%f", &broj[i]);
+        }
+    }
+      while(!feof(prva)){
+        for(i = 0; i < n;i++){
+            s += broj[i];
+        }
+        if(s > 0){
+            fprintf(druga,"%f",broj[i]);
+        }
+        s/=n;
+        zbirSrednjeVrednosti+=s;
+    }
+    printf("Zbir srednje vednisti je:%f",zbirSrednjeVrednosti);
+    fclose(prva);
+    fclose(druga);
 
 
     return 0;
