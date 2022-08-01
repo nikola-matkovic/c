b0a18586b822d99c392f042cf841cf752b9421a1
bimmer <tarikkucevic@gmail.com>
2022-08-01 18:25:52 +0200

    three files for all,positive and negative numbers

--- druga-zbirka/datoteke/5.18.c
+++ druga-zbirka/datoteke/5.18.c
@@ -10,11 +10,38 @@ int main(){
     FILE *svi;
     FILE *pozitivni;
     FILE *negativni;
+    int i,n;
+
     svi = fopen("svi.txt", "r");
     pozitivni = fopen("pozitivni.txt", "w");
     negativni = fopen("negativni.txt", "w");
     if(svi == NULL || pozitivni == NULL || negativni == NULL){
         printf("Greska");
     }
+    printf("Unesite n:");
+    scanf("%d",&n);
+    int broj[n];
+    while(!feof(svi)){
+    for(i = 0;i < n;i++){
+        fscanf(svi, "%d", &broj[i]);
+    }
+    }
+    while(!feof(svi)){
+        for(i = 0;i < n;i++){
+            if(broj[i] > 0){
+                fprintf(pozitivni,"%d",broj[i]);
+            }
+            else{
+                 fprintf(negativni,"%d",broj[i]);
+            }
+        }
+    }
+    fclose(svi);
+    fclose(pozitivni);
+    fclose(negativni);
+
+    
+
+    
     return 0;
 }
