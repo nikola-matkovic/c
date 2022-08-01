25521899f0b9c620b24ded997e5b55959f3f407a
bimmer <tarikkucevic@gmail.com>
2022-08-01 15:02:00 +0200

    number of lines and characters

--- druga-zbirka/datoteke/5.7.c
+++ druga-zbirka/datoteke/5.7.c
@@ -4,6 +4,27 @@
 #include<stdio.h>
 
 int main(){
-    
+    FILE *datoteka;
+    int brRedova = 1;
+    int brZnakova = 0;
+    long znak;
+    datoteka = fopen("podaci6.txt","r");
+    if(datoteka == NULL){
+        printf("Greska");
+    }
+    znak = getc(datoteka);
+    while(!feof(datoteka)){
+        if(znak == '\n'){
+            brRedova++;
+        }
+        else{
+            brZnakova++;
+        }
+    }
+    fclose(datoteka);
+    printf("Broj redova:%d",brRedova);
+    printf("Broj znakova:%d",brZnakova);
+
+
     return 0;
 }
