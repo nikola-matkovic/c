d8fb9428f4ef9255b37ff960a55d45f47ea986d2
bimmer <tarikkucevic@gmail.com>
2022-08-11 18:08:09 +0200

    ok

---
+++ rokovi/oktobar2-2020-02.c
@@ -0,0 +1,11 @@
+// 2. Napisati funkciju koja će kao argumente imati niz koji predstavlja cene automobila
+// (5-ti element za predstavlja cenu i-tog automobila), dužinu nira i bullet. Funkcija treba da vrati najveći
+// broj automobila koliko korisnik može da kupi sa prosledenim budhetom
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    
+    return 0;
+}
