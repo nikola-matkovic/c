033f903f5960404c828603449304bd45b7f9ad47
bimmer <tarikkucevic@gmail.com>
2022-08-13 21:27:33 +0200

    ok

---
+++ rokovi/avgust-2020-02.c
@@ -0,0 +1,29 @@
+// 2. Na programskom jeziku C napisati funkciju rotirajudesno koja vrši rotiranje prosledenog
+// iza dužine N za jedno mesto udesno. U glavnom programu umeti kvadratnu matricu i dimenzija
+// M&M i izvršiti rotiranje njenog oboda za jedno mesto u smeru kazaljke na satu (kao što je prikazano na primeru).
+// Primer
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int m,i,j;
+    int temp = 0;
+    printf("Unesite m:");
+    scanf("%d",&m);
+    int matrica[i][j];
+    for(i = 0;i < m;i++){
+        for(j = 0;j < m;j++){
+            scanf("%d",&matrica[i][j]);
+        }
+    }
+    printf("Rotiranje matrice:\n");
+    for(i = 0;i < m;i++){
+        for(j = i;j < m;j++){
+            if(i != j){
+                
+            }
+        }
+    }
+    return 0;
+}
