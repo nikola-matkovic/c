d68ada0265cc94dd30aa18faf4d3cdf6a3546624
bimmer <tarikkucevic@gmail.com>
2022-08-13 22:59:03 +0200

    ok

---
+++ rokovi/avgust-2020-03.c
@@ -0,0 +1,15 @@
+
+// 3.Na programskom jeziku C napisati strukturni program koji računa i prikazuje 
+// korisniku na standardni ilar povinu svake parcele. Podaci se nalaze o fajlu "parcele txt" 
+// i to naziv parcele a zatim XV levo, Ukoliko je površina parcele manja od hektara prikazati je
+// u arima (na primer 2.55a). levo, e parcela koje su zadate u sledećem redosledu dole-levo, dole-desno,
+// gore-desno, gore ukoliko je površina parcele bar jedan hektar prikazati je u hektarima (na primer 1.20h).
+// Parcele nisu pravougaonog oblika, pa njihovu površinu računati kao povrtinu dva trougla primenom Heronove formule.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    
+    return 0;
+}
