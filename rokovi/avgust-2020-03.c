a05050f4baa103ca84dd6e5a90ded3a48ec1d776
bimmer <tarikkucevic@gmail.com>
2022-08-15 15:14:58 +0200

    plot area expressed in hectares and ares

--- rokovi/avgust-2020-03.c
+++ rokovi/avgust-2020-03.c
@@ -9,14 +9,91 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define SIZE 100
+
 typedef struct parcela{
-    char nazv[30];
+    char naziv[30];
     int x;
     int y;
+    int povrsina;
 }PARCELA;
 
 int main(){
+    
     FILE *datoteka;
-    datoteka = fopen("parcele.txt", "r");
+    datoteka = fopen("parcele.txt", "w");
+    struct parcela  doleLevo;
+    struct parcela  doleDesno;
+    struct parcela  goreLevo;
+    struct parcela  goreDesno;
+    if(datoteka == NULL){
+        printf("Greska");
+        exit(1);
+    }
+    printf("Unesite naziv:");
+    scanf("%s",doleLevo.naziv);
+    printf("Unesite naziv:");
+    scanf("%s",doleDesno.naziv);
+    printf("Unesite naziv:");
+    scanf("%s",goreLevo.naziv);
+    printf("Unesite naziv:");
+    scanf("%s",goreDesno.naziv);
+
+    //dole levo
+    doleLevo.x = 500;
+    doleLevo.y = 700;
+    //dole desno
+    doleDesno.x = 4;
+    doleDesno.y = 15;
+    //gore levo
+    goreLevo.x = 3;
+    goreLevo.y = 5;
+    //gore desno
+    goreDesno.x = 600;
+    goreDesno.y = 40;
+
+    doleLevo.povrsina = (doleLevo.x + doleLevo.x)*(doleLevo.y + doleLevo.y)*1/2;
+    doleDesno.povrsina = (doleDesno.x + doleDesno.x)*(doleDesno.y + doleDesno.y)*1/2;
+    goreLevo.povrsina = (goreLevo.x + goreLevo.x)*(goreLevo.y + goreLevo.y)*1/2;
+    goreDesno.povrsina = (goreDesno.x + goreDesno.x)*(goreDesno.y + goreDesno.y)*1/2;
+
+    if(doleLevo.povrsina > 100){
+        doleLevo.povrsina /= 100;
+        fprintf(datoteka,"%s ima povrsinu:%dha\n",doleLevo.naziv,doleLevo.povrsina);
+    }
+    else{
+        fprintf(datoteka,"%s ima povrsinu:%da\n",doleLevo.naziv,doleLevo.povrsina);
+    }
+
+
+    if(doleDesno.povrsina > 100){
+        doleDesno.povrsina /= 100;
+        fprintf(datoteka,"%s ima povrsinu:%dha\n",doleDesno.naziv,doleDesno.povrsina);
+    }
+    else{
+        fprintf(datoteka,"%s ima povrsinu:%da\n",doleDesno.naziv,doleDesno.povrsina);
+    }
+
+    if(goreLevo.povrsina > 100){
+        goreLevo.povrsina /= 100;
+        fprintf(datoteka,"%s ima povrsinu:%dha\n",goreLevo.naziv,goreLevo.povrsina);
+    }
+    else{
+        fprintf(datoteka,"%s ima povrsinu:%da\n",goreLevo.naziv,goreLevo.povrsina);
+    }
+
+    if(goreDesno.povrsina > 100){
+        goreDesno.povrsina /= 100;
+        fprintf(datoteka,"%s ima povrsinu:%dha\n",goreDesno.naziv,goreDesno.povrsina);
+    }
+    else{
+        fprintf(datoteka,"%s ima povrsinu:%da\n",goreDesno.naziv,goreDesno.povrsina);
+    }
+    fclose(datoteka);
+
+
+
+  
+
     return 0;
 }
