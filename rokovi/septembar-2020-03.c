8197a59f79e5bac0e4527ccf41dcf0706ac0a2b1
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-15 19:59:11 +0200

    formated cd.txt file

---
+++ rokovi/septembar-2020-03.c
@@ -0,0 +1,14 @@
+/*
+u svakoj liniji tekstualne datoteke CD.txt nalaze se podaci o prodaji Compact diskova
+linije se sastoje od više kolona međusobno razdvojenih dvotačkom.
+prvoj koloni se nalazi naziv žanra cd-a koji može biti Jedan od vrednosti: 
+inostrana novokomponovana dečija klasična.
+u drugoj kolonije zadat broj prodatih primeraka za tekuću prodaju.
+u trećoj koloni dat je datum prodaje u obliku rednog broja između 1 početni dan intervala prodaje i 30 dan obrade podataka.
+potom slede podaci o nabavnoj ceni.  ti podaci sa zastoje od cene po primerku,
+ali za inostranu muziku dodatno se sada je i iznos carine po ukupnoj količini,
+dok se za novokomponovanu muziku dodatno se po primerku zadaje i iznos poreza na šund, potom slede podaci o prodajnoj ceni.
+broj linije u datoteci nije u napred poznat.
+Napisati program koji će odšampati na standradnom izlazu ukupne prihode od prodaje u intervalima od broja dana koje unosi korisnik sa tastature, i formatirati
+datotetu podaci.dat koja će biti u istom formatu kao datoteka cd.dat, ali će sadžati i podatke o prodaji diskova koji nisu žanra novokomponovane muzike.
+
