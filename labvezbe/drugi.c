ffbeef43501f471836d4733e73659da3d3c6c7e5
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-17 19:12:03 +0200

    drugi zadatak septembar popravni

--- labvezbe/drugi.c
+++ labvezbe/drugi.c
@@ -2,21 +2,15 @@
 int main(){
     int n, i, j, k=0; 
     scanf("%d", &n);
-    int A[n][n];
     for(j=0; j<n; j++){
         for(i=0; i<n; i++){
             if((i+j)>(n-2)){
                 k++;
-                A[i][j]=k;
+                printf("%d ", k);
             }
             else{
-                A[i][j] =0;
+                printf("%d ", 0);
             }
-        }
-    }
-    for(j=0; j<n; j++){
-        for(i=0; i<n; i++){
-            printf("%d ", A[i][j]);
         }
         printf("\n");
     }
