4af3e9a97da91fd1210b79c4ab7397cd5b873c45
bimmer <tarikkucevic@gmail.com>
2022-08-19 16:03:32 +0200

    ok

--- rokovi/jun-2020-02.c
+++ rokovi/jun-2020-02.c
@@ -55,27 +55,18 @@ void ispis(int niz[SIZE],int n){
 
 void umetanje(int niz[SIZE],int n,int b){
     int min = niz[0];
-    int temp2 = niz[n-1];
+    int temp = niz[0]; 
      for(int i = 0;i < n;i++){
         if(niz[i] < min){
             min = niz[i];
         }
     }
     n+=1;
-    for(int i = 0;i < n;i++){
-
+    for(int i = 1;i < n;i++){
+        niz[i] = niz[i + 1];
     }
-    for(int i = 0;i < n;i++){
-    if(niz[i] == min){
-        if(i == 0){
-            niz[n-1] = b;
-        }
-        niz[i - 1] = b;
-    }
-}
-int temp;
 
-  for(int i = 0;i < n-1;i++){
+for(int i = 0;i < n-1;i++){
     for(int j = 0;j < n-1;j++){
         if(niz[j] > niz[j + 1]){
             temp = niz[j];
