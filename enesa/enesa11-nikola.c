2ef6cd321c55533ba29355f993838f61244056c3
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-20 13:44:12 +0200

    finished 4.30

--- enesa/enesa11-nikola.c
+++ enesa/enesa11-nikola.c
@@ -5,13 +5,20 @@ int main(){
     int a;
     printf("unesite broj:");
     scanf("%d", &a);
-    int kopijaOdA = a;
-    for(int i = 0; i < kopijaOdA; i++){
+    int temp = a;
+    for(int i = a; i > 0; i--){
         for(int j = 0; j < a; j++){
             printf("%d ", a);
         }
         printf("\n");
         a--;
+    }
+    a = temp;
+    for(int i = 1; i <= a; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ", i);
+        }
+        printf("\n");
     }
     return 0;
 }
