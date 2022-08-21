a953c06ee7d0c46ce991b72ab74b2cd1ed2dc664
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-21 17:35:51 +0200

    za enesu

---
+++ erten/januar2022.c
@@ -0,0 +1,9 @@
+// 1. Nacrtati strukturni dijagram toka algoritma i na programskom jeziku C napisati program koji učitava
+// cele brojeve i štampa magične cifre sve dok korisnik ne unese broj koji nema nijednu magičnu cifru.
+// Cifra broja je magična ukoliko je njena vrednost jednaka poziciji na kojoj se nalazi.
+// Cifra jedinica se nalazi na poziciji 1, cifra desetica se nalazi na poziciji 2, cifra najveće
+// težine N-tocifrenog broja se nalazi na poziciji N.
+// Na primer:
+// U broju 71 cifra 1 je magična jer se nalazi na prvoj poziciji.
+// Broj 325 ima dve magične cifre, cifru 2 koja se nalazi na drugoj poziciji
+// i cifru 3 koja se nalazi na trećoj poziciji.
