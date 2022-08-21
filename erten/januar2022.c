e6204975df8df0f3d7dfad728a60502367dc92f7
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-21 18:31:36 +0200

    finished for only one number

--- erten/januar2022.c
+++ erten/januar2022.c
@@ -7,3 +7,31 @@
 // U broju 71 cifra 1 je magična jer se nalazi na prvoj poziciji.
 // Broj 325 ima dve magične cifre, cifru 2 koja se nalazi na drugoj poziciji
 // i cifru 3 koja se nalazi na trećoj poziciji.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    int modul = 10;
+    int delilac = 1;
+    scanf("%d", &n);
+    int i = 1;
+    while(1){
+        int cifra = n % modul / delilac;
+        modul = modul * 10;
+        delilac = delilac * 10;
+        if(cifra == i){
+            printf("%d ", cifra);
+        }
+        i++;
+
+
+
+
+        if(n / delilac  == 0){
+            break;
+        }
+    }
+    return 0;
+}
